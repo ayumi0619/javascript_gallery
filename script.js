db90a0bd5644edff0b00b0feeb2f4f0e8464b36d
ayumi0619 <108720099+ayumi0619@users.noreply.github.com>
2023-10-16 15:24:21 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
 // JavaScript
 
-//console.log(document.getElementById('myColor').value);
+const main = document.querySelector('#main-image');
+const thumbs = document.querySelectorAll('.thumb');
 
-//定数の宣言
-const color = document.getElementById('myColor');
-const text = document.getElementById('bodyText');
-
-//document.getElementById('bodyText').textContent = document.getElementById('myColor').value;
-function changeColor(){
-    // カラーコードを表示
-    if (color.value === '#ffffff') {
-      text.textContent = 'コード：' + color.value + '(white)';
-    } else if (color.value === '#000000') {
-      text.textContent = 'コード：' + color.value + '(black)';
-    } else {
-      text.textContent = 'コード：' + color.value;
+// 確認
+//console.log(main);
+//console.log(thumbs);
+thumbs.forEach(function(item, index) {
+    item.onmouseover = function() {
+      //console.log(this.dataset.image);
+    main.src = this.dataset.image;
     }
-  
-    // 背景色を変更
-      document.body.style.backgroundColor = color.value;
-    }
-    
-// カラーピッカーで色が選択されたら、changeColorを動作させる
-color.addEventListener('input', changeColor);
-
-
-
+  });
